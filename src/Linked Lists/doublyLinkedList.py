a1bf12f6f733d5a61afa97dd3022843e90f7b5c9
AJ Boyd <alanjaredboyd3@yahoo.com>
2023-08-31 22:30:18 -0400

    Implementation of a doubly linked list

---
+++ src/Linked Lists/doublyLinkedList.py
@@ -0,0 +1,10 @@
+"""
+auth: AJ Boyd
+date: 9/1/2023
+desc: this is an implementation of a doubly linked list with both a head and tail Node.
+    the key difference between a singly and doubly linked list is that a doubly linked list
+    has both a next and prev attribute, allowing each node to see its preceeding Node.
+    this program has two classes Node and LinkedList. linked lists are a fundamental 
+    data structure that features dynamic size, O(1) time complexity for insertion/deletion 
+    at the beginning and end, and a simple implementation method.
+"""
